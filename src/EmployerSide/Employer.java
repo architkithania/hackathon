473fb8302ca7883d6113cce7af657e0d2ae875c0
Stevenjohnlally <stevenjohnlally@gmail.com>
2018-11-18 01:50:48 +0800

    Added ComboBox for Education in EmployerSide

--- src/EmployerSide/Employer.java
+++ src/EmployerSide/Employer.java
@@ -9,6 +9,8 @@ import java.sql.SQLOutput;
 public class Employer {
     private static JLabel jobTitleLabel;
     private static JTextField jobTitleField;
+    private static JLabel educationLabel;
+    private static JComboBox educationComboBox;
     private static File file;
 
     public static void createListingsJSON(){
@@ -27,10 +29,9 @@ public class Employer {
     public static void main(String[] args){
         JFrame frame = new JFrame();
         frame.setSize(300, 250);
-        Panel panel = new Panel();
-        frame.add(panel);
-
         GridBagConstraints gc = new GridBagConstraints();
+        JPanel panel = new JPanel(new GridBagLayout());
+        frame.add(panel);
 
         Insets labelInsets = new Insets(0, 0, 0, 5);
         Insets blankInsets = new Insets(0,0,0,0);
@@ -47,26 +48,25 @@ public class Employer {
 
         jobTitleField = new JTextField(10);
         gc.gridx = 1;
-        gc.gridx = 0;
         gc.gridy = 0;
         gc.anchor = GridBagConstraints.LINE_START;
         gc.insets = blankInsets;
         panel.add(jobTitleField, gc);
 
-        jobTitleLabel = new JLabel("Job Title:");
+        educationLabel = new JLabel("Education:");
         gc.gridx = 0;
-        gc.gridy = 0;
+        gc.gridy = 1;
         gc.anchor = GridBagConstraints.LINE_END;
         gc.insets = labelInsets;
-        panel.add(jobTitleLabel, gc);
+        panel.add(educationLabel, gc);
 
-        jobTitleField = new JTextField(10);
+        String[] educationLevels = {"Diploma", "Bachelor's", "Master's", "PhD"};
+        educationComboBox = new JComboBox(educationLevels);
         gc.gridx = 1;
-        gc.gridx = 0;
-        gc.gridy = 0;
+        gc.gridy = 1;
         gc.anchor = GridBagConstraints.LINE_START;
         gc.insets = blankInsets;
-        panel.add(jobTitleField, gc);
+        panel.add(educationComboBox, gc);
 
         frame.setVisible(true);
 
