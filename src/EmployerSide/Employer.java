8a2054d8560502b5e7fea4f91c3102f026484145
Stevenjohnlally <stevenjohnlally@gmail.com>
2018-11-17 23:53:03 +0800

    Update Employer

--- src/EmployerSide/Employer.java
+++ src/EmployerSide/Employer.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Employer {
-    private static JLabel jobTitle;
+    private static JLabel jobTitleLabel;
+    private static JTextField jobTitleField;
 
     public static void main(String[] args){
         JFrame frame = new JFrame();
@@ -15,17 +16,40 @@ public class Employer {
         GridBagConstraints gc = new GridBagConstraints();
 
         Insets labelInsets = new Insets(0, 0, 0, 5);
-        Insets blankInset = new Insets(0,0,0,0);
+        Insets blankInsets = new Insets(0,0,0,0);
 
         gc.weightx = 1.0;
         gc.weighty = 1.0;
 
-        jobTitle = new JLabel("Job Title:");
+        jobTitleLabel = new JLabel("Job Title:");
         gc.gridx = 0;
         gc.gridy = 0;
         gc.anchor = GridBagConstraints.LINE_END;
         gc.insets = labelInsets;
-        panel.add(jobTitle, gc);
+        panel.add(jobTitleLabel, gc);
+
+        jobTitleField = new JTextField(10);
+        gc.gridx = 1;
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = blankInsets;
+        panel.add(jobTitleField, gc);
+
+        jobTitleLabel = new JLabel("Job Title:");
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = labelInsets;
+        panel.add(jobTitleLabel, gc);
+
+        jobTitleField = new JTextField(10);
+        gc.gridx = 1;
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = blankInsets;
+        panel.add(jobTitleField, gc);
 
         frame.setVisible(true);
 
