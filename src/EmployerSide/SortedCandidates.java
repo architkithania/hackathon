48a916dc62700ee9330ed4230ed77c0ec05facec
architkithania <archit.kithania@gmail.com>
2018-11-18 04:20:37 +0800

    updating SortCandidates

--- src/EmployerSide/SortedCandidates.java
+++ src/EmployerSide/SortedCandidates.java
@@ -1,10 +1,23 @@
 package EmployerSide;
 
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SortedCandidates {
 
     int [] ids;
 
     SortedCandidates(int interviewSlots, String [] priorityList, String jobTitle) {
+        try {
+            Gson gson = new Gson();
+            Scanner jobListing = new Scanner(new File("job_listings/" + jobTitle + ".json"));
 
+
+        } catch (FileNotFoundException ex) {
+            ex.getMessage();
+        }
     }
 }
